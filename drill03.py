bf4b7bb5e98e9de25b0bbb354d047d9cba52b272
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2024-09-24 11:49:15 +0900

    원그리기 사각형 그리기 함수

---
+++ drill03.py
@@ -0,0 +1,18 @@
+from pico2d import*
+
+open_canvas()
+
+grass=load_image('grass.png')
+character = load_image('character.png')
+
+def run_circle():
+    print('circle')
+    pass
+def run_rectangle():
+    print('rectangle')
+    pass
+
+
+while True:
+    run_rectangle()
+    run_circle()

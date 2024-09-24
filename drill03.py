0a6bec5601f9e012a6a076b20de02c3b3f9cffb2
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2024-09-24 12:33:58 +0900

    윗줄이동 완성, 드로우 보이 함수 생성

--- drill03.py
+++ drill03.py
@@ -4,8 +4,15 @@ open_canvas()
 
 grass=load_image('grass.png')
 boy= load_image('character.png')
+def draw_boy(x,y):
+    clear_canvas_now()
+    boy.draw_now(x,y)
+    delay(0.1)
+
 def run_top():
     print('top')
+    for x in range(0,800,10):
+        draw_boy(x,550)
     pass
 def run_right():
     print('right')
@@ -22,19 +29,14 @@ def run_circle():
     r=300
     cx=800//2 ##화면 크기의 절반
     cy=600//2
-
     for degree in range(0,360,3):
         theta=math.radians(degree)
         x=r*math.cos(theta)+cx
         y=r*math.sin(theta)+cy
+        draw_boy(x,y)
 
-        clear_canvas_now()
-        boy.draw_now(x,y)
-        delay(0.1)
-
-
+       
 def run_rectangle():
-    print('rectangle')
     run_top()
     run_right()
     run_bottom()
@@ -43,6 +45,7 @@ def run_rectangle():
 
 
 while True:
-    run_rectangle()
     run_circle()
+    run_rectangle()
+
     break##임시로 넣음

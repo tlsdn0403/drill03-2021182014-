fbfdbd88a992db1e27fb691a3a7b47ef62c4ebc4
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2024-09-24 12:12:15 +0900

    원 운동 구현 완료

--- drill03.py
+++ drill03.py
@@ -1,15 +1,24 @@
 from pico2d import*
-
+import math
 open_canvas()
 
 grass=load_image('grass.png')
 boy= load_image('character.png')
 
 def run_circle():
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(0.1)
 
+    r=300
+    cx=800//2 ##화면 크기의 절반
+    cy=600//2
+
+    for degree in range(0,360,3):
+        theta=math.radians(degree)
+        x=r*math.cos(theta)+cx
+        y=r*math.sin(theta)+cy
+
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.1)
     pass
 def run_rectangle():
     print('rectangle')
